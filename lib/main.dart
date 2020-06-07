9b1a2b9db482fe982e85b7636b923ed8f32fadbe
TakumaYamashita <dia_yamataku@yahoo.co.jp>
2020-06-07 15:33:18 +0900

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Takuma Yamashita'),
+        title: Text('Takuma Yamashitaaaaaaaaaaa'),
         actions: <Widget>[
           Icon(Icons.add),
           Icon(Icons.share),
